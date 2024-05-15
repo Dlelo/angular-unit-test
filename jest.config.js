ba7932b3a977200ece58ec7d6d57a3c574ead376
Donattah <d.ajuang@carepay.com>
2024-05-16 00:00:53 +0300

    use-jest-test-runner update config

--- jest.config.js
+++ jest.config.js
@@ -1,10 +1,11 @@
 module.exports = {
   // ...other options
-  moduleFileExtensions: ['ts', 'html', 'js', 'json', 'mjs'],
+  moduleFileExtensions: ['js', 'jsx', 'ts', 'tsx', 'json'],
   resolver: 'jest-preset-angular/build/resolvers/ng-jest-resolver.js',
   transformIgnorePatterns: ['node_modules/(?!.*\\.mjs$)'],
   transform: {
     '^.+\\.(ts|js|mjs|html|svg)$': 'jest-preset-angular',
   },
+  
   
 };
